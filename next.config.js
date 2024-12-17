19fd7396ac471e45d1d98fee33339658887d3c6b
Jose Martinez <josemartinez@Joses-MacBook-Pro.local>
2024-12-17 10:30:06 -0600

    fixes

--- next.config.js
+++ next.config.js
@@ -14,4 +14,12 @@ module.exports = {
       },
     ]
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:3000/api/:path*',
+      },
+    ];
+  },
 }
